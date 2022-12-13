a80864df0a90ebdfe845744e6ac76676325bc2f0
Eliacobe <114414469+Eliacobe@users.noreply.github.com>
2022-12-13 16:30:08 +0000

    definition and word displayed

--- dictionary.js
+++ dictionary.js
@@ -9,25 +9,31 @@ btn.addEventListener("click", () => {
     const urls = url + inpWord;
     fetch(urls)
     .then((resp) => resp.json())
-    
+
     .then((data)=> {
         console.log(data);
-        result.innerHTML =`
-        <div class="word">
-      <h3>${inpWord}</h3>
-      <button onclick="playSound()">
-        <i class="fa-solid fa-volume-high"></i>
-      </button>
-    </div>
-    <div class="details">
-      <p>${data[0].meanings[0].partOfSpeech}</p>
-      <p>${data[0].phonetic}</P>
-    </div>
-    <p class="meaning">${data[0].meanings[0].definitions[0].definition}</p>
-    <p class="example">${data[0].meanings[0].definitions[0].example }</p>`;
-    sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
-    
-    
+        wordHeading.innerHTML = `
+        ${inpWord}`
+        definition.innerHTML = `
+        ${data[0].meanings[0].definitions[0].definition}
+        `
+    //     result.innerHTML =`
+    //     <div class="word">
+    //   <h3>${inpWord}</h3>
+    //   <button onclick="playSound()">
+    //     <i class="fa-solid fa-volume-high"></i>
+    //   </button>
+    // </div>
+    // <div class="details">
+    //   <p>${data[0].meanings[0].partOfSpeech}</p>
+    //   <p>${data[0].phonetic}</P>
+    // </div>
+    // <p class="meaning">${data[0].meanings[0].definitions[0].definition}</p>
+    // <p class="example">${data[0].meanings[0].definitions[0].example }</p>`;
+    // sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+
+
+
     })
     .catch(() => {
         result.innerHTML = `<h3>No result Found`
@@ -36,4 +42,3 @@ btn.addEventListener("click", () => {
 function playSound(){
     sound.play();
 }
-
