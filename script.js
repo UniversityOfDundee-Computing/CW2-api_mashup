c949e554838f0bbf0168066e30f7eb8040211698
Sujalthaku <114414537+Sujalthaku@users.noreply.github.com>
2022-12-15 23:07:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,70 @@
-const url1 = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const result = document.getElementById("result");
-const sound = document.getElementById("sound");
-const btn = document.getElementById("search-btn");
-const heading = document.getElementById("wordHeading");
+const fromText = document.querySelector(".from-text"),
+toText = document.querySelector(".to-text"),
+exchageIcon = document.querySelector(".exchange"),
+selectTag = document.querySelectorAll("select"),
+icons = document.querySelectorAll(".row i");
+translateBtn = document.querySelector("button"),
 
-btn.addEventListener("click", () => {
-  let inpWord = document.getElementById("inp-word").value;
-
-  console.log(inpWord);
-  const url2 = url1 + inpWord;
-
-  fetch(url2)
-    .then((resp) => resp.json())
-    .then((data) => {
-      console.log(data);
-      heading.innerHTML = `
-      <div class="h text-center">
-    <h3>${inpWord}</h3>
-        <p>${data[0].meanings[0].definitions[0].definition}</p>
-        </div>`;
-    })
-    .catch(() => {
-      heading.innerHTML = `<h3>No result Found</h3>`;
-    });
-
-  $.ajax({
-    method: "GET",
-    url: "https://api.api-ninjas.com/v1/thesaurus?word=" + inpWord,
-    headers: { "X-Api-Key": "3tbIyhQrxCdB1UWGIlW2xg==rk2T9t3OF3jyk5WA" },
-    contentType: "application/json",
-    success: function (result) {
-      console.log(result);
-
-      word.innerHTML =
-        result.synonyms.length === 0
-          ? "Not Found "
-          : result.synonyms.filter((item, index) => index < 5);
-
-      // synonyms.innerHTML = `${result.synonyms[0]}, ${result.synonyms[1]}, ${result.synonyms[2]}, ${result.synonyms[3]}, ${result.synonyms[4]}`
-    },
-    error: function ajaxError(jqXHR) {
-      console.error("Error: ", jqXHR.responseText);
-    },
-  });
+selectTag.forEach((tag, id) => {
+    for (let country_code in countries) {
+        let selected = id == 0 ? country_code == "en-GB" ? "selected" : "" : country_code == "hi-IN" ? "selected" : "";
+        let option = `<option ${selected} value="${country_code}">${countries[country_code]}</option>`;
+        tag.insertAdjacentHTML("beforeend", option);
+    }
 });
-function playSound() {
-  sound.play();
-}
+
+exchageIcon.addEventListener("click", () => {
+    let tempText = fromText.value,
+    tempLang = selectTag[0].value;
+    fromText.value = toText.value;
+    toText.value = tempText;
+    selectTag[0].value = selectTag[1].value;
+    selectTag[1].value = tempLang;
+});
+
+fromText.addEventListener("keyup", () => {
+    if(!fromText.value) {
+        toText.value = "";
+    }
+});
+
+translateBtn.addEventListener("click", () => {
+    let text = fromText.value.trim(),
+    translateFrom = selectTag[0].value,
+    translateTo = selectTag[1].value;
+    if(!text) return;
+    toText.setAttribute("placeholder", "Translating...");
+    let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
+    fetch(apiUrl).then(res => res.json()).then(data => {
+        toText.value = data.responseData.translatedText;
+        data.matches.forEach(data => {
+            if(data.id === 0) {
+                toText.value = data.translation;
+            }
+        });
+        toText.setAttribute("placeholder", "Translation");
+    });
+});
+
+icons.forEach(icon => {
+    icon.addEventListener("click", ({target}) => {
+        if(!fromText.value || !toText.value) return;
+        if(target.classList.contains("fa-copy")) {
+            if(target.id == "from") {
+                navigator.clipboard.writeText(fromText.value);
+            } else {
+                navigator.clipboard.writeText(toText.value);
+            }
+        } else {
+            let utterance;
+            if(target.id == "from") {
+                utterance = new SpeechSynthesisUtterance(fromText.value);
+                utterance.lang = selectTag[0].value;
+            } else {
+                utterance = new SpeechSynthesisUtterance(toText.value);
+                utterance.lang = selectTag[1].value;
+            }
+            speechSynthesis.speak(utterance);
+        }
+    });
+});
